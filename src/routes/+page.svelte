3342579b6ff494cfd2777bc13a7aff6c4de4f0a0
Hamidreza.behzadi <hamidreza.behzadi@Admins-MacBook-Pro-3.local>
2024-08-02 12:31:15 +0330

    feat: add tailwind css

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,2 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<h1 class="text-amber-600">Welcome to SvelteKit</h1>
+<p class="font-bold text-red-700">Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
