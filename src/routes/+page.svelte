f2b32a63bcfe20c7901a2e1123d4486dcaaeac8c
Hamidreza.behzadi <hamidreza.behzadi@Admins-MacBook-Pro-3.local>
2024-08-02 13:46:22 +0330

    feat: add fonts and colors

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,4 +1,4 @@
-<h1 class="text-amber-600">Welcome to SvelteKit</h1>
+<h1 class="font-kalam text-amber-600">Welcome to SvelteKit</h1>
 <p class="font-bold text-red-700">
   Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation
 </p>
