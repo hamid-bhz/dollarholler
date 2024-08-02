718a4be83ba8779e6b5d8e4608d5c7aeab5c37f9
Hamidreza.behzadi <hamidreza.behzadi@Admins-MacBook-Pro-3.local>
2024-08-02 13:21:46 +0330

    feat: format code

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,3 +3,9 @@
 	Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation
 </p>
 <button class="bg-sky-700 px-4 py-2 text-white hover:bg-sky-800 sm:px-8 sm:py-3"> click </button>
+
+<style lang="postcss">
+	button {
+		@apply text-sm;
+	}
+</style>
